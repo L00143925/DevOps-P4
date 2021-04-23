6b541ae6192df37e66495ecf879ddd9b5405f459
L00143925 <78552943+L00143925@users.noreply.github.com>
2021-04-23 16:11:29 +0100

    Add test branch
    adding testbranch

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class TestJava{
+public static void main(String[] args) {
+ name();
+ System.out.println("hello there");
+
+}
+    public static void name() {
+       System.out.println("my name is howfar" );
+    
+}
+}
